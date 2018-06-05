9aaf62f37604ac54abb87c7d4295307cc3d89c8e
Michael.Xiu <997611764@qq.com>
2018-06-06 00:35:19 +0800

    fix weight_buffer

--- convplex.srcs/sources_1/new/Weight_Buffer.v
+++ convplex.srcs/sources_1/new/Weight_Buffer.v
@@ -2,84 +2,46 @@
 
 
 // Weight_Buffer : store 9 * 16bits input Weight for convolution
-// Data inputs according to the clk and Write_en sequentially, data outputs according to the clk and Read_en concurrently
+// Data inputs according to the clk and en=1 sequentially, lasting for 9 clock cycles; data outputs continuously and concurrently
 module Weight_Buffer (
-    Write_en,
-    Read_en,
-    clk,
-    rst,
-    din,
-    dout0,
-    dout1,
-    dout2,
-    dout3,
-    dout4,
-    dout5,
-    dout6,
-    dout7,
-    dout8
+    input en,
+    input clk,
+    input [15:0] din,
+    output  [15:0] dout0,
+    output  [15:0] dout1,
+    output  [15:0] dout2,
+    output  [15:0] dout3,
+    output  [15:0] dout4,
+    output  [15:0] dout5,
+    output  [15:0] dout6,
+    output  [15:0] dout7,
+    output  [15:0] dout8
 );
-    
-    input Write_en, Read_en, clk, rst;
-    input [15:0] din;
-    output reg [15:0] dout0, dout1, dout2, dout3, dout4, dout5, dout6, dout7, dout8;
     
     reg [15:0] Memory [15:0];  // storing the input data array 16*16bits, using 9*16bits in fact
     reg [4:0] count = 4'b0;       // record the current position for input data in the array
 
-    always @ (posedge clk, posedge rst)
-    begin
-        if (rst) 
-            begin
-                 Memory[0] <= 16'b0;
-                 Memory[1] <= 16'b0;
-                 Memory[2] <= 16'b0;
-                 Memory[3] <= 16'b0;
-                 Memory[4] <= 16'b0;
-                 Memory[5] <= 16'b0;
-                 Memory[6] <= 16'b0;
-                 Memory[7] <= 16'b0;
-                 Memory[8] <= 16'b0;
-                 Memory[9] <= 16'b0;
-                 Memory[10] <= 16'b0;
-                 Memory[11] <= 16'b0;
-                 Memory[12] <= 16'b0;
-                 Memory[13] <= 16'b0;
-                 Memory[14] <= 16'b0;
-                 Memory[15] <= 16'b0;                              
-              
-                 count <= 4'b0;
-                 
-                 dout0 <= 16'b0;
-                 dout1 <= 16'b0;
-                 dout2 <= 16'b0;
-                 dout3 <= 16'b0;
-                 dout4 <= 16'b0;
-                 dout5 <= 16'b0;
-                 dout6 <= 16'b0;
-                 dout7 <= 16'b0;
-                 dout8 <= 16'b0;
-             end
-        else if (Write_en)   
-            begin
-                if (count < 4'b1001)
-                    begin
-                        Memory[count] <= din;
-                        count = count + 4'b1;
-                    end
+    always @ (posedge clk)     // input according to clk
+        begin
+            if (en==1 && count < 4'b1001)
+                begin
+                    Memory[count] <= din;
+                    count <= count + 1;
                 end
-          else if (Read_en)
-            begin
-                dout0 <= Memory[0];
-                dout1 <= Memory[1];
-                dout2 <= Memory[2];
-                dout3 <= Memory[3];
-                dout4 <= Memory[4];
-                dout5 <= Memory[5];
-                dout6 <= Memory[6];
-                dout7 <= Memory[7];
-                dout8 <= Memory[8];
-            end
-     end       
+            if (en==0)
+                begin
+                    count <= 4'b0;
+                end
+        end
 
+    assign dout0 = Memory[0];
+    assign dout1 = Memory[1];
+    assign dout2 = Memory[2];
+    assign dout3 = Memory[3];
+    assign dout4 = Memory[4];
+    assign dout5 = Memory[5];
+    assign dout6 = Memory[6];
+    assign dout7 = Memory[7];
+    assign dout8 = Memory[8];
+    
 endmodule
