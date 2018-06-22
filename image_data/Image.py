91565e8415c7f8cba5522ced60e5996637f78b59
Michael.Xiu <997611764@qq.com>
2018-06-22 21:50:30 +0800

    image processing-fix2

--- image_data/Image.py
+++ image_data/Image.py
@@ -1,11 +1,18 @@
-import os
 import pickle
-
+import numpy as np
+import os
 import load_CIFAR_gray
 import matplotlib.pyplot as plt
-import numpy as np
 
 # 30:plane 4:car
-X_train = load_CIFAR_gray.get_cifar_data([30], 'plane.txt')
-load_CIFAR_gray.gray_plot(30)
-load_CIFAR_gray.output_plot('plane_output1.txt')
+X_train = load_CIFAR_gray.get_cifar_data_coe([4],'car.coe')
+# load_CIFAR_gray.gray_plot(30)
+
+
+
+# X_train = load_CIFAR_gray.get_cifar_data_txt([4],'car.txt')
+# plt.subplot(1,2,2)
+# load_CIFAR_gray.output_plot('car_output1.txt')
+# plt.subplot(1,2,1)
+# load_CIFAR_gray.output_plot('car_output2.txt')
+# plt.show()
