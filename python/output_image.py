8fc5d33c38515014cc1a722f12efbb196608e5e4
Patrick22414 <yangsy9@mail2.sysu.edu.cn>
2018-06-29 17:34:11 +0800

    small python optimize

--- python/output_image.py
+++ python/output_image.py
@@ -1,13 +1,13 @@
-import numpy as np
 from matplotlib import pyplot as plt
+from numpy import loadtxt
 
 
 def show_output():
-    image = np.loadtxt("../convplex.sim/sim_1/behav/xsim/output.txt")
-    image[(image > 4095) * (image < 32768)] = 4095
+    image = loadtxt("../convplex.sim/sim_1/behav/xsim/output.txt", )[0:900].reshape(30, 30)
+
     image[(image > 32767)] = 0
-    image = (image[0:900] / 4096).reshape(30, 30)
-    print(np.max(image))
+    image[(image > 4095)] = 4095
+
     plt.imshow(image, cmap="gray")
     plt.show()
 
